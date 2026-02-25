007c86ac13f42040b67b6a74a7be566e75e7e879
Vishnu Mukundan <vishnumukundan@Mac-554.lan>
2026-02-25 17:08:41 -0500

    sortformer + ECAPA

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ echo "Installing NeMo (--no-deps to respect pinned versions)..."
 pip install --no-deps 'git+https://github.com/NVIDIA/NeMo.git@main#egg=nemo_toolkit[asr]'
 
 echo "Installing remaining NeMo sub-deps..."
-pip install hydra-core omegaconf lightning torchmetrics editdistance \
+pip install hydra-core omegaconf "lightning>2.2.1,<=2.4.0" torchmetrics editdistance \
     jiwer sentencepiece transformers librosa webdataset lhotse braceexpand \
     nv-one-logger-core nv-one-logger-training-telemetry \
     nv-one-logger-pytorch-lightning-integration
